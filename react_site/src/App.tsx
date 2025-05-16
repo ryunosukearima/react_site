af71a1bb817954384a7ff22845e3e7b2a4eaf29d
ryunosuke_arima <ryunosuke.arima125>
2025-05-16 15:16:38 +0900

    App.tsxの追加

---
+++ react_site/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='h-28 w-28 bg-red-500'></div>
+    </div>
+  )
+}
+
+export default App
