1c6d276645dcfe4c0ba8023c688aa8ce104ad24f
ryunosuke_arima <ryunosuke.arima125>
2025-05-17 23:02:56 +0900

    クラス名を付けた

--- react_site/src/App.tsx
+++ react_site/src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <div className='h-28 w-28 bg-red-500'></div>
+    <div className='w-auto h-70 bg-amber-100'>
     </div>
   )
 }
