ce9d9b84cc938f8290c285eee2aaa520da85230e
ryunosuke_arima <ryunosuke.arima125>
2025-05-18 20:33:27 +0900

    flex学習

--- react_site/src/App.tsx
+++ react_site/src/App.tsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 
 function App() {
   return (
-    <div className='w-auto h-70 bg-amber-100'>
+    <div>
+      <div className='flex'>
+        <div className="w-14 flex-1 text-center">01</div>
+        <div className="w-64 flex-1 text-center">02</div>
+        <div className="w-32 flex-1 text-center">03</div>
+      </div>
     </div>
   )
 }
