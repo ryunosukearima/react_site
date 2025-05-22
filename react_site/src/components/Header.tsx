467e04b7afe496de28fd14f67774cf092c74e9db
ryunosuke_arima <ryunosuke.arima125>
2025-05-22 09:10:31 +0900

    HeaderComponent追加

---
+++ react_site/src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+function Header() {
+  return (
+    <div>
+      <div className='flex'>
+        <div className='bg-gradient-to-r from-green-400 via-cyan-900 to-blue-500 h-10 w-10'></div>
+        <div className='bg-gradient-to-r from-green-400 via-cyan-900 to-blue-500 h-10 w-10'></div>
+        <div className='bg-gradient-to-r from-green-400 via-cyan-900 to-blue-500 h-10 w-10'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
