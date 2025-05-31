b2501e2a8424164c99802b4a03ef0a1bd195f408
ryunosuke_arima <ryunosuke.arima125>
2025-05-31 20:58:05 +0900

    枠組みの作成

--- react_site/src/components/music/Music.tsx
+++ react_site/src/components/music/Music.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import Audio_ornament from './audio_ornament/Audio_ornament'
 
 const Music = () => {
   return (
-    <div>  
-      <Audio_ornament/> 
+    <div>
+      <div className='flex'>
+        <div className="m-auto text-center">DJMIX</div>
+      </div>
+      <div className='flex justify-center mt-4'>
+        <div className='w-1/2 bg-gray-200 p-4 rounded-lg shadow-lg'>
+          <h2 className='text-xl font-bold mb-4'>Music Player</h2>
+          {/* Add your music player component here */}
+          <p>Music player will be implemented here.</p>
+        </div>
+      </div>
+       <div className='flex'>
+        <div className="m-auto text-center">DJMIX</div>
+      </div>
+      <div className='flex justify-center mt-4'>
+        <div className='w-1/2 bg-gray-200 p-4 rounded-lg shadow-lg'>
+          <h2 className='text-xl font-bold mb-4'>Music Player</h2>
+          {/* Add your music player component here */}
+          <p>Music player will be implemented here.</p>
+        </div>
+      </div>
+       <div className='flex'>
+        <div className="m-auto text-center">DJMIX</div>
+      </div>
+      <div className='flex justify-center mt-4'>
+        <div className='w-1/2 bg-gray-200 p-4 rounded-lg shadow-lg'>
+          <h2 className='text-xl font-bold mb-4'>Music Player</h2>
+          {/* Add your music player component here */}
+          <p>Music player will be implemented here.</p>
+        </div>
+      </div>
     </div>
     )
 }
